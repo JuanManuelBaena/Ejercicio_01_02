ac6b68052fbba005673c45cc08ec6828a2616bfc
Juan Manuel Baena <jbaeurb3009@g.educaand.es>
2025-09-24 22:20:19 +0200

    Ejercicio 2 finalizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("Elena Nito Del Bosque");
+        System.out.println("Elena Nito Del Bosque");
+        System.out.println("Calle Rodahuevos, 69 - Guaro (Málaga) - España");
+        System.out.println("555 123456");
     }
 }
